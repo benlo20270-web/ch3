15ae45b83a24a7bbb906293e9f806f54cf15f9ae
benlo20270-web <benlo20270@gmail.com>
2026-09-16 13:34:53 -0700

    Add files via upload

--- Convert.java
+++ Convert.java
@@ -4,3 +4,8 @@ import java.util.Scanner;
  public class Convert {
 	public static void main(String[] args) {
 	Scanner in = new Scanner(System.in);
+	System.out.print("Total Hours, Minutes, Seconds? ");
+	final int S_PER_M = 60;
+	final int M_PER_H = 60;
+	double S = in.nextDouble();
+	int seconds
