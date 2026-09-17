006d796c2a8625db0f40475fd2e395efb3ce402b
benlo20270-web <benlo20270@gmail.com>
2026-09-17 12:45:38 -0700

    Add files via upload

--- Convert.java
+++ Convert.java
@@ -4,8 +4,16 @@ import java.util.Scanner;
  public class Convert {
 	public static void main(String[] args) {
 	Scanner in = new Scanner(System.in);
-	System.out.print("Total Hours, Minutes, Seconds? ");
+	System.out.print("Total second?");
 	final int S_PER_M = 60;
 	final int M_PER_H = 60;
-	double S = in.nextDouble();
-	int seconds
+	int S = in.nextInt();
+	int minute = (int) (S / S_PER_M);
+	int hour = minute / M_PER_H;
+	int remH = S % M_PER_H;
+	int Rhour = minute % M_PER_H;
+	System.out.printf("%d Hours, %d Minutes, %d Seconds", hour, Rhour, remH);
+	
+}
+}
+	
